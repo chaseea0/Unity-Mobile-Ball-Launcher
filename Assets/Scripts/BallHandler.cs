b6b7e37b04c8916ee5283ca08a5af567ba6f10f6
Evan Chase <chaseea0@gmail.com>
2024-11-03 14:57:08 -0500

    feat(Video 5): read touch input
    - add BallHandler and script
    - check if the user is pressing the screen; if they are, log the position of the click

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            Debug.Log(touchPosition);
+        }
+    }
+}
