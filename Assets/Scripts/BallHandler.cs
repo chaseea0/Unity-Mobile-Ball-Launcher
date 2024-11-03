de93255ac1375c0fcfe0bd28c50b8b060848ddd7
Evan Chase <chaseea0@gmail.com>
2024-11-03 15:31:15 -0500

    feat(Video 8): add logic to "let go" of the ball
    - add a check to update to see if you had been dragging the ball around and it's just been released
    - add logic to remove the references to the ball rigid body and spring joint at the appropriate time
    - incorporate a detaching delay to be used when invoking "DetachBall" so that the physics doesn't immediately stop and drop to the ground

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -7,7 +7,10 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D currentBallRigidBody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay;
     private Camera mainCamera;
+    private bool isDragging;
 
     void Start()
     {
@@ -16,17 +19,38 @@ public class BallHandler : MonoBehaviour
 
     void Update()
     {
+        if (currentBallRigidBody == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidBody.bodyType = RigidbodyType2D.Dynamic;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
 
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
         currentBallRigidBody.bodyType = RigidbodyType2D.Kinematic;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
         currentBallRigidBody.position = worldPosition;
 
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidBody.bodyType = RigidbodyType2D.Dynamic;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
